835c270c4dae95bc6f4382ece2e7f6dbf4c9ec0e
HermadesCGCO <hermadescgco@disroot.org>
2021-02-15 14:11:40 +0000

    This should prevent difficulty jumps

--- blockchain.js
+++ blockchain.js
@@ -40,6 +40,7 @@ class Blockchain {
 	    const { timestamp, lastHash, hash, nonce, difficulty, data } = chain[i]
 
 	    const actualLastHash = chain[i-1].hash
+	    const lastDifficulty = chain[i-1].difficulty
 
 	    if (lastHash !== actualLastHash) {
 		return false
@@ -48,6 +49,10 @@ class Blockchain {
 	    const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty)
 
 	    if (hash !== validatedHash) {
+		return false
+	    }
+
+	    if (Math.abs(lastDifficulty - difficulty) > 1) {
 		return false
 	    }
 	}
