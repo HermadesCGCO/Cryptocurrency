61feb697eaebeb19d8e85b0cce22112b5b0a97f5
HermadesCGCO <hermadescgco@disroot.org>
2021-02-15 00:38:18 +0000

    Added difficulty and nonce

--- block.test.js
+++ block.test.js
@@ -7,11 +7,17 @@ describe("Block", () => {
     const lastHash = "foo-hash"
     const hash = "bar-hash"
     const data = ['blockchain', 'data']
+
+    const nonce = 1
+    const difficulty = 1
+
     const block = new Block({
 	timestamp,
 	lastHash,
 	hash,
-	data
+	data,
+	nonce,
+	difficulty
     })
 
     it("has a timestamp, lastHash, hash and data properly", () => {
@@ -19,6 +25,8 @@ describe("Block", () => {
 	expect(block.lastHash).toEqual(lastHash)
 	expect(block.hash).toEqual(hash)
 	expect(block.data).toEqual(data)
+	expect(block.nonce).toEqual(nonce)
+	expect(block.difficulty).toEqual(difficulty)
     })
 
     describe("genesis()", () => {
@@ -56,7 +64,20 @@ describe("Block", () => {
 
 	it("creates a SHA-256 hash based on the proper inputs", () => {
 	    expect(minedBlock.hash)
-		.toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data))
+		.toEqual(
+		    cryptoHash(
+			minedBlock.timestamp,
+			minedBlock.nonce,
+			minedBlock.difficulty,
+			lastBlock.hash,
+			data
+		    )
+		)
+	})
+
+	it("sets a `hash` that matches the difficulty", () => {
+	    expect(minedBlock.hash.substring(0, minedBlock.difficulty))
+		.toEqual('0'.repeat(minedBlock.difficulty))
 	})
     })
 })
--- config.js
+++ config.js
@@ -1,7 +1,11 @@
+const INITIAL_DIFFICULTY = 3
+
 const GENESIS_DATA = {
     timestamp: 1,
     lastHash: "-----",
     hash: "hash-one",
+    difficulty: INITIAL_DIFFICULTY,
+    nonce: 0,
     data: "Dean Martin (born Dino Paul Crocetti; June 7, 1917 – December 25, 1995) was an American singer, actor, and comedian. One of the most popular and enduring American entertainers of the mid-20th century, Martin was nicknamed \"The King of Cool\"."
 }
 
